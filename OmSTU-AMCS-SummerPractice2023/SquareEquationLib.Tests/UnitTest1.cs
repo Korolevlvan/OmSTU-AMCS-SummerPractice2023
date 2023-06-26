44c59b171efc847578370299288c0040ed4265b0
Azazel <Ivan76098@gmail.com>
2023-06-26 18:04:48 +0600

    Creation_Test

---
+++ OmSTU-AMCS-SummerPractice2023/SquareEquationLib.Tests/UnitTest1.cs
@@ -0,0 +1,68 @@
+using Xunit;
+using SquareEquationLib;
+namespace SquareEquationLib.Tests
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(0.0 , 2.0, 3.0)]
+        [InlineData(Double.NaN, 2.0, 30.0)]
+        [InlineData(1.0 , Double.NaN, 30.0)]
+        [InlineData(1.0 , 2.0, Double.NaN)]
+        [InlineData(Double.PositiveInfinity , 2.0, 30.0)]
+        [InlineData(1.0 , Double.PositiveInfinity, 30.0)]
+        [InlineData(1.0 , 2.0, Double.PositiveInfinity)]
+        [InlineData(Double.NegativeInfinity , 2.0, 30.0)]
+        [InlineData(1.0 , Double.NegativeInfinity, 30.0)]
+        [InlineData(1.0 , 2.0, Double.NegativeInfinity)]
+        public void UnsuitableValues(double a, double b, double c)
+        {
+            bool flag = false;
+            try
+            {
+
+                double[] Value = SquareEquation.Solve(a, b, c);
+                Console.WriteLine(Value);
+            }
+            catch(ArgumentException)
+            {
+                flag = true;
+            }
+            Assert.True(flag);
+        }
+        [Fact]
+        public void EpsilonValues()
+        {
+            double a = 1.0;
+            double b = 0.0;
+            double c = (Convert.ToDouble(1e-9)/5.0);
+            double[] Value1 = SquareEquation.Solve(a, b, c);
+            double[] Value2 = SquareEquation.Solve(a, b, -c);
+            bool flag = false;
+            if(Value1.Length == 1 && Value2.Length == 1)flag = true;
+            Assert.True(flag);
+        }
+        [Fact]
+        public void OneSolutions()
+        {
+            double a = 1.0;
+            double b = 0.0;
+            double c = 0.0;
+            double[] Value = SquareEquation.Solve(a, b, c);
+            bool flag = false;
+            if(Value.Length == 1)flag = true;
+            Assert.True(flag);
+        }
+        [Fact]
+        public void ZeroSolutions()
+        {
+            double a = 1.0;
+            double b = 0.0;
+            double c = 10.0;
+            double[] Value = SquareEquation.Solve(a, b, c);
+            bool flag = false;
+            if(Value.Length == 0)flag = true;
+            Assert.True(flag);
+        }
+    }
+}
