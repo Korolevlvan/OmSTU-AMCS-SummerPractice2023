aba022ecf79e164f598db2c5b8802cc12c4081ec
Azazel <Ivan76098@gmail.com>
2023-06-30 01:13:42 +0600

    Revert "Добавлен тест для сервиса 1"
    This reverts commit 669024d5a87ff65dbe1d2496958cd52f843129e3.

--- SalonLib.Test/ServiceLocatorTest.cs
+++ SalonLib.Test/ServiceLocatorTest.cs
@@ -2,12 +2,5 @@ namespace SalonLib.Test;
 
 public class ServiceLocatorTest
 {
-    [Fact]
-    public void Service1Test()
-    {
-        var expected = "В кабинете";
-        var actual = SalonLib.ServiceLocator.GetService("Читать стихи");
-        Assert.Equal(expected, actual);
-    }
 
 }
