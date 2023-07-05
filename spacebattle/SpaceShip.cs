e9c1fa37c5c11267028f83d72752361cb7dde4ac
Azazel <Ivan76098@gmail.com>
2023-07-05 21:31:55 +0600

    Ресурсы

--- spacebattle/SpaceShip.cs
+++ spacebattle/SpaceShip.cs
@@ -54,4 +54,49 @@ public class SpaceShip
             return value;
         }
 }
+public class Pool<SHpool> where SHpool : new()
+{
+    Queue<SHpool> Ships = new Queue<SHpool>();
+    int count;
+    public Pool(int count)
+    {
+        this.count = count;
+
+        for (int i = 0; i < count; i++)
+        {
+            Ships.Enqueue(new SHpool());
+        }
+    }
+    public SHpool Get_Object()
+    {
+        return Ships.Dequeue();
+    }
+    public void Release_Object(SHpool ship)
+    {
+        if(Ships.Count == count)
+            throw new InvalidOperationException("Pool is Full!!");
+        else
+        {
+            Ships.Enqueue(ship);
+        }
+    }
+}
+public class PoolGuard<SHpool> : IDisposable where SHpool : new()
+{
+    Pool<SHpool> pool;
+    SHpool ship;
+    public PoolGuard(Pool<SHpool> pool)
+    {
+        ship = pool.Get_Object();
+        this.pool = pool;
+    }
+    public void Dispose()
+    {
+        pool.Release_Object(ship);
+    }
+    public SHpool Get_Object()
+    {
+        return ship;
+    }
+}
 }
